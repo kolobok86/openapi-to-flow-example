33491884754361ca7303596f836bdc9279086c98
vkolokolov <vsevolod.kolokolov@smartbear.com>
2019-12-22 15:17:18 +0300

    generate-flow-type: added formatting for downloaded JSON specs

--- get-remote-spec.js
+++ get-remote-spec.js
@@ -5,29 +5,23 @@ const path = require('path');
 const { spawn, fork } = require('child_process');
 
 const specsConfig = require('./apiSpecs/_specsConfig.json');
-const keys = require('./secret-keys.json');
+const secKeys = require('./secret-keys.json');
 
-// const specUrl = process.env.npm_package_apiSpecUrl;
-// const specVersion = process.env.npm_package_apiSpecVersion;
-// const specFullUrl = `${specUrl}/${ specVersion || ''}`;
-// const specFilePath = './apiSpecs/petStore.yaml';
 
 const specsDirPath = './apiSpecs';
 const typesDirPath = './types';
+const specEncoding = 'utf8';  // frankly, this is incorrect and encoding should be inferred from response Content-Type;
+                              // but let it be for sake of simplicity
+const jsonStringifySpace = 2; // use 2 spaces when formatting fetched spec in JSON format
+
 
 specsConfig.apiSpecs.forEach(async (spec) => {
   const specUrl = spec.url;
   const specVersion = spec.version;
   const specFileName = spec.fileName;
   const specFullUrl = `${specUrl}${ specVersion ? '/' + specVersion : '' }`;
-  // const specFilePath = path.join(specsDirPath, specFileName);
 
-  // const result = await fetchRemoteSpec(specFullUrl, specFileName)
-
-  // console.log('written file:', result);
-
-  // await generateFlowType();
-
+  // process specs from specsConfig asynchronously
   fetchAndGenType(specFullUrl, specFileName);
 });
 
@@ -71,75 +65,70 @@ async function fetchRemoteSpec (specUrl, specFileName, resultObj) {
   const promise = new Promise((resolve, reject) => {
     const webClient = (specUrl.startsWith('https://')) ? https : http;
     webClient.get(specUrl, (res) => {
-      if (res.statusCode === 200) {
-        let fetchError = false;
 
-        let specFileExt;
-        switch (res.headers['content-type']) {
-          case 'application/yaml':
-            specFileExt = '.yaml';
-            break;
-          case 'application/json':
-            specFileExt = '.json';
-            break;
-          default:
-            console.warn('Warning: Content-Type of response is not "application/yaml" neither "application/json". Assuming that spec is in JSON format');
-            specFileExt = '.json';
-        }
+      res.setEncoding(specEncoding);
+      let resBody = '';
 
-        const specFilePath = path.join(specsDirPath, specFileName) + specFileExt;
-        const specFileWriteStream = fs.createWriteStream(specFilePath);
+      res.on('data', (chunk) => {
+        resBody += chunk;
+      });
 
-        specFileWriteStream.on('error', (err) => {
-          fetchError = true;    // will be used in `specFileWriteStream.on('finish', . . . )` (see below),
-                                // that is emitted no matter if write succeeded or not
-          specFileWriteStream.close();
-          reject(err);
-        });
+      res.on('end', () =>{
+        if (res.statusCode === 200) {
+          let fetchError = false;
+          let dataToWrite = resBody;
 
-        specFileWriteStream.on('finish', () => {
-          if (!fetchError) {
-            specFileWriteStream.close();
+          let specFileExt;
+          switch (res.headers['content-type']) {
+            case 'application/yaml':
+              specFileExt = '.yaml';
+              break;
+            case 'application/json':
+              specFileExt = '.json';
+              // format JSON spec to printable format, for simpler grabbing changes between versions
+              try {
+                const specDataObj = JSON.parse(resBody);
+                dataToWrite = JSON.stringify(specDataObj, null, jsonStringifySpace);
+              }
+              catch (err) {
+                console.error(`Error on parsing JSON structure of spec fetched from ${specUrl}`);
+                reject(err);
+              }
+              break;
+            default:
+              console.warn('Warning: Content-Type of response is not "application/yaml" neither "application/json". Assuming that spec is in JSON format');
+              specFileExt = '.json';
+          }
+
+          const specFilePath = path.join(specsDirPath, specFileName) + specFileExt;
+          fs.writeFile(specFilePath, dataToWrite, specEncoding, (err) => {
+            if (err) {
+              console.error(`Error on writing fetched spec to file ${specFilePath}`);
+              reject (err);
+            }
+
             console.log(`Fetching spec from ${specUrl} succeded, spec saved in ${specFilePath}`);
             resultObj['apiSpecPath'] = specFilePath;
-
             resolve(specFilePath);
-          }
-        });
+          })
 
-        res.pipe(specFileWriteStream);
-      }
-      else {
-        res.setEncoding('utf8');  // frankly, this is incorrect and encoding should be inferred from response Content-Type;
-                                  // but let it be for sake of simplicity
-        let resBody = '';
-        res.on('data', (chunk) => {
-          resBody += chunk;
-        });
-
-        res.on('end', () =>{
-          reject(new Error(`Error on fetching spec ${specUrl}:\n\tHTTP error code ${res.statusCode}\n\tServer response:${resBody}`));
-        });
-      }
+        }
+        else {
+          // we treat response code different from 200 as error case
+          reject(new Error(`Error on fetching spec ${specUrl}:\n\tHTTP error code ${res.statusCode}\n\tServer response: ${resBody}`));
+        }
+      });
 
       res.on('error', (err) => {
-        // console.error(`Error on fetching spec from remote host: ${specUrl}`);
-
+        console.error(`Error on fetching spec from remote host: ${specUrl}`);
         reject(err);
       });
     });
 
   })
   .catch(err => {
-    // console.error(err.message);
     resultObj['error'] = err;
   });
 
-  // const result = await promise;
-  // return result;
   await promise;
 }
-
-
-
-
